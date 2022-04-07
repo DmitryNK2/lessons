fa0cf2f741d2371acab4b98f3367f62f601b26d9
DmitryNK <kuz25dmi@yandex.ru>
2022-04-07 22:32:45 +0400

    lesson02_continuation

---
+++ lesson_2_continuation/script.js
@@ -0,0 +1,27 @@
+
+let money = 11000;
+let income = "фриланс";
+let addExpenses = "Интернет, такси, коммуналка";
+let deposit = true;
+let mission = 500000;
+let period = 12;
+let budgetMonth;
+let expenses1;
+let expenses2;
+let amount1;
+let amount2;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+console.log("цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase());
+
+let butgetDay = money / 30;
+
+console.log(butgetDay);
